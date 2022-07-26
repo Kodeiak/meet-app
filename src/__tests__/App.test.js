4b03ca4140724f1693d37d816273a02d6a0551c0
Paul Wandrey <88623843+Kodeiak@users.noreply.github.com>
2022-07-26 10:26:42 -0500

    add and code for remaining first feature passing tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,14 +2,22 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 import EventList from "../EventList";
+import CitySearch from "../CitySearch";
 
 // create new test group/scope called <App /> component
 describe("<App /> component", () => {
+  let AppWrapper;
+  beforeAll(() => {
+    // use shallow (w/o DOM or childen components) rendering API from Enzyme in order to run test
+    AppWrapper = shallow(<App />);
+  })
 
   test("render list of events", () => { //test description
-    // use shallow (w/o DOM or childen components) rendering API from Enzyme in order to run test
-    const AppWrapper = shallow(<App />); 
     expect(AppWrapper.find(EventList)).toHaveLength(1);  // actual test > there should only be one Event List component
+  });
+
+  test("render CitySearch", () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
 });
